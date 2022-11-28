031c4dc97c1d56510a506b8792079d67823fdf8b
zbs <zhangbshu@gmail.com>
2022-11-28 22:58:33 +0800

    aoc06 init

---
+++ aoc06/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+}
+
+// 需要找到上下左右之最
+// 遍历每个点, 寻找距离最近的点(i, j) -> (i - 1, j) (i, j - 1) (i + 1, j) (i, j + 1)
+fn part1(input: &str) {
+    let grid = [[0; 400]; 400];
+    let (left, right, top, bottom) = (0, 0, 0, 0);
+    let (i, j) = (0, 0);
+    while i < grid.len() {
+        while j < grid[i].len() {
+            
+        }
+    }
+}
